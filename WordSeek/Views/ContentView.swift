44e8eacc05515bf1922a07bf6ba228acd54f453f
Jacob Holloway <jholloway1342@sdsu.edu>
2024-03-06 22:52:17 -0800

    Re-aligned Words and word search

--- WordSeek/Views/ContentView.swift
+++ WordSeek/Views/ContentView.swift
@@ -6,39 +6,52 @@ struct ContentView: View {
     @State private var newGrid = [[Character]]()
     @State private var selectedCategory: String = ""
     @State private var selectedList: WordList = WordList(category: "", words: [])
-    var selected = wordLists.randomElement()!
-    var pdfGenerator = PDFGenerator() // Assume this is your class for generating PDFs
-
-    var body: some View {
-        
-        HStack() {
-            VStack{
-                Button("Generate New Grid") {
-                    self.generateNewGrid()
-                }
-                .buttonStyle(PlainButtonStyle())
-                .padding()
-                .frame(minWidth: 100, maxWidth: 150, minHeight: 40)
-                .background(RoundedRectangle(cornerRadius: 8).fill(Color.accentColor))
-                .foregroundColor(Color.white)
-                
-                if showingGrid {
-                    NewGridView(grid: self.newGrid, category: self.selectedCategory, createPDFAction: { })
-                        .padding()
-                }
-            }
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-        }
-    }
+    @State private var gridWords = [String]()
+    // Save the words used
+//    var selected = wordLists.randomElement()!
     
     func generateNewGrid() {
         let selected = wordLists.randomElement()!
         self.selectedCategory = selected.category
         self.newGrid = WordGridGenerator(words: Array(selected.words.shuffled().prefix(10).map { $0.uppercased() }), row: 15, column: 15).generate() ?? []
+        self.gridWords = Array(selected.words.shuffled().prefix(25).map { $0.uppercased() })
         self.showingGrid = true
     }
     
+    var body: some View {
+        
+        
+        
+        HStack() {
+            VStack{
+               
+                HStack{
+                    Button("Generate New Search") {
+                        self.generateNewGrid()
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(minWidth: 100, maxWidth: 150, minHeight: 40)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.accentColor))
+                    .foregroundColor(Color.white)
+                    
+                    Button("Generate PDF") {
+                        self.generateNewGrid()
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(minWidth: 100, maxWidth: 150, minHeight: 40)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.accentColor))
+                    .foregroundColor(Color.white)
+                    
+                }
+                if showingGrid {
+                    NewGridView(grid: self.newGrid, category: self.selectedCategory, words: self.gridWords) // Use self.gridWords here
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        }
+    }
+        
    
     }
 
--- WordSeek/Views/NewGridView.swift
+++ WordSeek/Views/NewGridView.swift
@@ -1,10 +1,3 @@
-//
-//  NewGridView.swift
-//  WordSeek
-//
-//  Created by Jacob Holloway on 3/4/24.
-//
-
 import SwiftUI
 import AppKit
 
@@ -12,47 +5,61 @@ struct NewGridView: View {
     
     let grid: [[Character]]
     let category: String
-    var createPDFAction: () -> Void
+    let words: [String]  // Add this line to accept the words
     
     var body: some View {
-            // Define the columns for the new grid
-            let columns = Array(repeating: GridItem(.flexible(), spacing: 5), count: grid.first?.count ?? 0)  // Use the actual grid size or a default value
-        
-
-            VStack{
-                ScrollView {
-                    
-                    Text(category)  // Display the category
-                        .font(.headline)// Set the font to headline for better visibility
-                    
-                        .padding(.top)
-                    LazyVGrid(columns: columns, spacing: 2) {
-                        ForEach(0..<grid.count, id: \.self) { rowIndex in
-                            ForEach(0..<grid[rowIndex].count, id: \.self) { columnIndex in
-                                Text(String(grid[rowIndex][columnIndex]))
-                                    .frame(width: 30, height: 30)  // Adjust size for better visibility
-                                    .background(Color.white)
-                                    .foregroundColor(Color.black)
-                                    .id("\(rowIndex)-\(columnIndex)")
+        HStack {
+            HStack{// Grid view
+                VStack {
+                    ScrollView {
+                        Text(category)  // Display the category
+                            .font(.headline)  // Set the font to headline for better visibility
+                            .padding(.top)
+                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 5), count: grid.first?.count ?? 0), spacing: 2) {
+                            ForEach(0..<grid.count, id: \.self) { rowIndex in
+                                ForEach(0..<grid[rowIndex].count, id: \.self) { columnIndex in
+                                    Text(String(grid[rowIndex][columnIndex]))
+                                        .frame(width: 30, height: 30)  // Adjust size for better visibility
+                                        .background(Color.white)
+                                        .foregroundColor(Color.black)
+                                        .id("\(rowIndex)-\(columnIndex)")
+                                }
                             }
                         }
-                      }
                     }
-                .padding([.trailing,.leading,.top])
-
+                    .padding([.trailing, .leading, .top])
                 }
-            
-            .foregroundColor(Color.black)
-            .background(Color.white)
             }
+            
+             // Add a spacer to push the words list to the right
+            
+            // Word list
+            VStack(alignment: .leading) {
+                 // Add space at the top for alignment with the grid
+                Text("Words")  // You might want to style this header
+                    .font(.headline)
+                    .padding([.bottom, .top])  // Add padding on top and bottom for spacing
+                ForEach(words, id: \.self) { word in
+                    Text(word)
+                        
+                }
+                Spacer()
+            }
+            .padding([.trailing, .leading, .top])
+            
         }
+        .foregroundColor(Color.black)
+        .background(Color.white)
         
-
+    }
+    
+}
 
 struct Previews: PreviewProvider {
     static var previews: some View {
-        let gameGrid: [[Character]] = Grid.init()
+        let gameGrid: [[Character]] = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]]  // Sample grid for preview purposes
         let category: String = "Animals"
-        NewGridView(grid: gameGrid, category: category, createPDFAction: { })
+        let words: [String] = ["Dog", "Cat", "Bat"]  // Sample words for preview purposes
+        NewGridView(grid: gameGrid, category: category, words: words)
     }
 }
